caef94aaf3d023253f1c340b3542098ba9e1f88d
Don Park <don@donpark.org>
2012-02-12 17:22:35 +0700

    Session controller handles oauth callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Crowdstarter::Application.routes.draw do
   get "dashboard/explain"
 
+  match "auth/:provider/callback" => "sessions#create" 
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'dashboard#explain'
