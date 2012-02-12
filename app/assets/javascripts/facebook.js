c615c50fb9f98542e4bbf911bf169b7a5310dc0e
Don Park <don@donpark.org>
2012-02-12 16:40:57 +0700

    listen for facebook auth event

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -7,6 +7,16 @@ function facebook_setup(appid) {
             xfbml      : true,
             oauth      : true,
           });
+          FB.Event.subscribe('auth.login', function(r)
+              {
+                  console.log(r.status);
+
+                  if ( r.status === 'connected' )
+                  {
+                      // a user has logged in
+                  }
+              }
+          );
         };
 
         (function(d){
