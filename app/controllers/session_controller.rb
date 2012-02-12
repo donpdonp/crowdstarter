13e03291b9070dcf166c68f0e2a3997ccd17d45c
Don Park <don@donpark.org>
2012-02-12 17:55:14 +0700

    finish the session with a redirect

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,6 +2,7 @@ class SessionController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
+    redirect_to params[:redirect_to] || :root
   end
 
   def auth_hash
