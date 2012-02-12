b06df0fc9887fa64b4a32bd60e87b224656946cd
Don Park <don@donpark.org>
2012-02-12 19:16:47 +0700

    login test

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe SessionController do
+
+  it "should lookup a user" do
+    new_user = mock_model(User)
+    User.should_receive(:find_or_create_from_auth_hash).and_return(new_user)
+    post :create
+    response.should redirect_to(:root)
+  end
+
+end
