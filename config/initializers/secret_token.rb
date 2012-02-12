dfe038e379ade39d059ca1d71384c8bbdcf0acd9
Don Park <don@donpark.org>
2012-02-12 11:46:28 +0700

    skeleton rails 2.3 psql app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdstarter::Application.config.secret_token = '749548a19b3759a1eaebdfa4fa06c7d638760a7fe4d161aaa874c87925c08e15ef1adcd94c7be1108ff4a7d24c50acd87bb8ed46b5aabb5a42d2711d0c5d0c01'
