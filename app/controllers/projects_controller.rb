44f6c9a926b3c138d87b300a75c55e3bbc9e4617
Don Park <don@donpark.org>
2012-02-13 15:45:01 +0700

    create a new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,13 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new
+  end
+
+  def create
+    project = Project.create(params[:project])
+    redirect_to project || :root
+  end
+  
   def show
     @project = Project.find(params[:id])
   end
