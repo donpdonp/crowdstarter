f239dc4d8ed1ece0d7d03d1dd96e30ae242baa26
Don Park <don@donpark.org>
2012-02-13 20:07:23 +0700

    progress bar

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ActiveRecord::Base
   has_many :contributions
 
   validates_presence_of :name, :funding_due, :amount, :user_id
+
+  def collected
+    contributions.sum(&:amount)
+  end
+
+  def percent_complete
+    collected / amount
+  end
 end
