1687c119f80e85745ce24de27574fe0240ea30c0
Don Park <don@donpark.org>
2012-02-13 15:27:14 +0700

    images on projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120212115358) do
+ActiveRecord::Schema.define(:version => 20120213081741) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120212115358) do
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "image_url"
   end
 
   create_table "users", :force => true do |t|
