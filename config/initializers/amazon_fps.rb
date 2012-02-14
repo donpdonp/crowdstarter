c658deccdb237ba8f0fdaaa861aac6b3ab9b3765
Don Park <don@donpark.org>
2012-02-14 17:39:40 +0700

    missing file

---
+++ config/initializers/amazon_fps.rb
@@ -0,0 +1,3 @@
+FPS = Boomerang.new( SETTINGS['aws']['access_key'],
+                     SETTINGS['aws']['access_secret'],
+                     SETTINGS['aws']['sandbox'] )  # use sandbox (false sends to production)
