9ac3702509d8678c237092896c99b6dba8e5d00b
Kurt Sussman <kls@merlot.com>
2012-02-20 11:28:34 -0800

    Use mocks for testing the donpdonp way

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -2,18 +2,28 @@ require "spec_helper"
 
 describe Notifications do
   describe "thanks" do
-    # create user, project, and contribution
-    let(:mail) { pending Notifications.thanks(@contribution) }
+
+    before(:each) do
+      @user = mock_model(User, {username: "testuser", email: "em@ail.com"})
+      @project = mock_model(Project, {name: "My Test Project"})
+      @contribution = Contribution.new
+
+      @contribution.should_receive(:user).twice.and_return(@user)
+      @contribution.should_receive(:project).and_return(@project)
+      @contribution.should_receive(:amount).and_return(29.95)
+    end
 
     it "renders the headers" do
-      pending
+      mail = Notifications.thanks(@contribution)
+
       mail.subject.should eq("Thanks for your donation!")
-      mail.to.should eq([@contribution.user.email])
+      mail.to.should eq(["em@ail.com"])
       mail.from.should eq(["from@example.com"])
     end
 
     it "renders the body" do
-      pending
+      mail = Notifications.thanks(@contribution)
+
       mail.body.encoded.should match("Hi")
     end
   end
