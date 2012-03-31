66cb645c7005fa7bd8bcf6e02ffb116f90a93e2d
Don Park <don@donpark.org>
2012-03-30 18:15:18 -0700

    backgrounded

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,15 +1,13 @@
 class GithubController < ApplicationController
   def commit
     payload = JSON.parse(params["payload"])
-    logger.info("github params: #{payload}")
     out = shell("restart.sh")
-    logger.info("github pull output: #{out}")
     render :nothing => true
   end
 
   private
   # easy to mock
   def shell(cmd)
-    `/bin/sh #{cmd}`
+    `/bin/sh #{cmd} &`
   end
 end
