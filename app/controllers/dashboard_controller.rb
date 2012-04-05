8bb7cf914b3c30aa1eeb4b71c41ec1048156c1e9
Don Park <don@donpark.org>
2012-04-05 16:30:06 -0700

    no menu items left.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def explain
+    @projects_count = Project.count
+    @contributions_amount = Contribution.successful.sum(:amount)
     @example_projects = Tag.find_by_name("frontpage").projects
   end
 end
