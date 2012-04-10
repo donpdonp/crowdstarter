e7c218dac959133da75d75d8f065f3f8c0610499
Don Park <don@donpark.org>
2012-04-10 14:41:04 -0700

    states on Project

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ApplicationController
 
   private
   def projects_tagged(tag)
-    Tag.find_by_name(tag).projects.order(:funding_due)
+    Tag.find_by_name(tag).projects.where(:workflow_state => 'fundable').
+        order(:funding_due)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -11,6 +11,16 @@ class Project < ActiveRecord::Base
   validates_presence_of :name, :funding_due, :amount, :user_id
 
   workflow do
+    state :editable do
+      event :publish, :transitions_to => :fundable
+    end
+    state :fundable do
+      event :close, :transitions_to => :funded
+    end
+    state :funded do
+      event :disburse, :transitions_to => :disbursed
+    end
+    state :disbursed
   end
 
   def collected
@@ -25,7 +35,7 @@ class Project < ActiveRecord::Base
     collected / amount
   end
 
-  def disburse
+  def collect_contributions
     contributions.successful.each do |contrib|
       begin
         payment = FPS.pay( caller_reference:      "#{id}-#{rand(100)}",
