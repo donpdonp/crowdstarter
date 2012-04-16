d8db36843c7583971a9beb7bd70cb30e9bc139ec
Don Park <don@donpark.org>
2012-04-16 14:35:07 -0700

    capybara is starting to work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def explain
-    @example_projects = projects_tagged("frontpage")
+    @example_projects = projects_tagged("frontpage").order(:funding_due).limit(10)
   end
 
   def jobs
@@ -9,7 +9,6 @@ class DashboardController < ApplicationController
 
   private
   def projects_tagged(tag)
-    Tag.find_by_name(tag).projects.where(:workflow_state => 'fundable').
-        order(:funding_due)
+    Tag.find_by_name(tag).projects.where(:workflow_state => 'fundable')
   end
 end
--- spec/integration/sign_in_spec.rb
+++ spec/integration/sign_in_spec.rb
@@ -1,7 +1,11 @@
+require 'spec_helper'
+
 describe "Sign in", :type => :request do
   it "signs in with facebook" do
-    #within(".user-nav") do
-    #  click_link
-    #end
+    visit '/'
+    #puts page.html
+    within(".user-nav") do
+      #click_link "facebook-login"
+    end
   end
 end
