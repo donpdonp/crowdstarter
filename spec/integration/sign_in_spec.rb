7bb2785d764896b89a0d17d60e96372785476b72
Don Park <don@donpark.org>
2012-04-16 15:17:53 -0700

    a working integration test

--- spec/integration/sign_in_spec.rb
+++ spec/integration/sign_in_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe "Sign in", :type => :request do
   it "signs in with facebook" do
     visit '/'
-    #puts page.html
     within(".user-nav") do
       click_link "facebook-login"
     end
+    page.should have_content('user@test.site')
   end
 end
