44721b4dca9b78d76dc67a42c565398a7895e6ae
Don Park <don@donpark.org>
2012-05-01 14:45:40 -0700

    editable guard and activity log

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,14 +1,31 @@
 class RewardsController < ApplicationController
   before_filter :require_login, :only => [:new, :create, :edit, :update]
+  before_filter :editable_project, :only => [:new, :create, :edit, :update]
 
   def new
-    @project = Project.find(params[:project_id])
     @reward = Reward.new
   end
 
   def create
-    @project = current_user.projects.find(params[:project_id])
-    @project.rewards.create(params[:reward])
+    unless @project.editable?
+      flash[:error] = "Project is not editable"
+    end
+    reward = @project.rewards.create(params[:reward])
+    if reward.valid?
+      @project.activities.create({:detail => "Reward added at $#{"%0.2f" % reward.amount}",
+                                  :code => "reward",
+                                  :user => current_user})
+    else
+      flash[:error] = "Reward creation failed."
+    end
     redirect_to @project
+  end
+
+  def editable_project
+    @project = current_user.projects.find(params[:project_id])
+    unless @project.editable?
+      flash[:error] = "Project is not editable"
+      redirect_to @project
+    end
   end
 end
