8af89efb5880c8fd0e5672de1a3b1cb888b3c14e
Don Park <don@donpark.org>
2012-05-07 15:16:52 -0700

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Crowdstarter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'dashboard#explain'
+  root :to => 'dashboard#landing'
 
   # See how all your routes lay out with "rake routes"
 end
