3ee3f7feb8763fb907f486a37d4975417fa11e3a
Don Park <don@donpark.org>
2012-05-08 09:49:17 -0700

    landing page spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -5,7 +5,7 @@ describe DashboardController do
   it "should display the explanation page" do
     subject.should_receive(:projects_tagged).with("frontpage", 10).and_return([])
     get :landing
-    response.should render_template("dashboard/explain")
+    response.should render_template("dashboard/landing")
   end
 
 end
