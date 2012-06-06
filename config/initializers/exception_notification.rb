927659babb84ff5668164dfad9c963b0826ce293
Don Park <don@donpark.org>
2012-06-06 10:16:19 -0700

    no exeption emails in development

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,4 +1,6 @@
-Crowdstarter::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "",
-  :sender_address => SETTINGS["exception_notifier"]["from"],
-  :exception_recipients => SETTINGS["exception_notifier"]["recipients"]
+unless Rails.env.development?
+  config.middleware.use ::ExceptionNotifier,
+    :email_prefix => "",
+    :sender_address => SETTINGS["exception_notifier"]["from"],
+    :exception_recipients => SETTINGS["exception_notifier"]["recipients"]
+end
