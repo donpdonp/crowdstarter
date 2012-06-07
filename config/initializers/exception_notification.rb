fbac39e53b9718158ad374efa673271491c9af63
Don Park <don@donpark.org>
2012-06-07 10:23:39 -0700

    turns out tests dont like plain 'config'

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,5 +1,5 @@
 unless Rails.env.development?
-  config.middleware.use ::ExceptionNotifier,
+  Crowdstarter::Application.config.middleware.use ::ExceptionNotifier,
     :email_prefix => "",
     :sender_address => SETTINGS["exception_notifier"]["from"],
     :exception_recipients => SETTINGS["exception_notifier"]["recipients"]
