356b4d31d08fa6af083e5e1fd98e101d6c66c106
Don Park <don@donpark.org>
2012-06-12 13:51:55 -0700

    more wepay connect bits

--- config/initializers/wepay_oauth.rb
+++ config/initializers/wepay_oauth.rb
@@ -2,4 +2,7 @@ WEPAY = OAuth2::Client.new(SETTINGS.payment_gateways.wepay.client.id,
                            SETTINGS.payment_gateways.wepay.client.secret,
                                  {:site => "https://stage.wepay.com/",
                                   :authorize_url => "/v2/oauth2/authorize",
-                                  :token_url => "/v2/oauth2/token"})
+                                  :token_url => "/v2/oauth2/token",
+                                  :connection_opts => {
+                                    :ssl => {:verify=> !Rails.env.development?}
+                                    }})
