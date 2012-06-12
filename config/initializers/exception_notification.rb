a148c0835829928636b3811a48c1874a8682a8ea
Don Park <don@donpark.org>
2012-06-12 12:44:30 -0700

    multiple payment gateways

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,6 @@
 unless Rails.env.development?
   Crowdstarter::Application.config.middleware.use ::ExceptionNotifier,
     :email_prefix => "",
-    :sender_address => SETTINGS["exception_notifier"]["from"],
-    :exception_recipients => SETTINGS["exception_notifier"]["recipients"]
+    :sender_address => SETTINGS.exception_notifier.from,
+    :exception_recipients => SETTINGS.exception_notifier.recipients
 end
--- config/initializers/wepay_oauth.rb
+++ config/initializers/wepay_oauth.rb
@@ -1,5 +1,5 @@
-WEPAY = OAuth2::Client.new(SETTINGS['wepay']['client']['id'],
-                           SETTINGS['wepay']['client']['secret'],
+WEPAY = OAuth2::Client.new(SETTINGS.payment_gateways.wepay.client.id,
+                           SETTINGS.payment_gateways.wepay.client.secret,
                                  {:site => "https://stage.wepay.com/",
                                   :authorize_url => "/v2/oauth2/authorize",
                                   :token_url => "/v2/oauth2/token"})
