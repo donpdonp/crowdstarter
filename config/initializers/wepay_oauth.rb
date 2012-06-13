0af9c09ac208023749bea8f82e292b28339c0fa9
Don Park <don@donpark.org>
2012-06-13 14:08:08 -0700

    wepay sandbox configuration

--- config/initializers/wepay_oauth.rb
+++ config/initializers/wepay_oauth.rb
@@ -1,6 +1,6 @@
 WEPAY = OAuth2::Client.new(SETTINGS.payment_gateways.wepay.client.id,
                            SETTINGS.payment_gateways.wepay.client.secret,
-                                 {:site => "https://stage.wepay.com/",
+                                 {:site => SETTINGS.payment_gateways.wepay.sandbox ? "https://stage.wepay.com/" : "https://www.wepay.com/",
                                   :authorize_url => "/v2/oauth2/authorize",
                                   :token_url => "/v2/oauth2/token",
                                   :connection_opts => {
