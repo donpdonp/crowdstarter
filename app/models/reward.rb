6fc67e2b9f8ac1325be94ede8ff2b82e5119ab54
Don Park <don@donpark.org>
2012-06-13 15:25:03 -0700

    reward descriptions can use html

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,4 +1,6 @@
 class Reward < ActiveRecord::Base
-  # attr_accessible :title, :body
+  # attr_accessible :amount, :description
   validates :amount, :description, :presence => true
+
+  html_fragment :description, :scrub => :escape
 end
