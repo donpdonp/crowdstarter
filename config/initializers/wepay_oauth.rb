a24bb4750fa256f8bbd7aa91f26e41834560795a
Don Park <don@donpark.org>
2012-07-11 11:04:53 -0700

    better wepay endpoints

--- config/initializers/wepay_oauth.rb
+++ config/initializers/wepay_oauth.rb
@@ -1,6 +1,6 @@
 WEPAY = OAuth2::Client.new(SETTINGS.payment_gateways.wepay.client.id,
                            SETTINGS.payment_gateways.wepay.client.secret,
-                                 {:site => SETTINGS.payment_gateways.wepay.sandbox ? "https://stage.wepay.com/" : "https://www.wepay.com/",
+                                 {:site => SETTINGS.payment_gateways.wepay.sandbox ? "https://stage.wepayapi.com/" : "https://wepayapi.com/",
                                   :authorize_url => "/v2/oauth2/authorize",
                                   :token_url => "/v2/oauth2/token",
                                   :connection_opts => {
