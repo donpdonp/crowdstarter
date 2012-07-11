25138e1a9f789c1866162edff7dad80b661a9a34
Don Park <don@donpark.org>
2012-07-11 11:03:51 -0700

    handle a checkout that has already begun

--- app/controllers/gateways/wepay_controller.rb
+++ app/controllers/gateways/wepay_controller.rb
@@ -1,29 +1,45 @@
 class Gateways::WepayController < ApplicationController
   def checkout
     contribution = current_user.contributions.find(params[:contribution_id].to_i)
-    project_owner = contribution.project.user
-    wp_params = {
-           :account_id => project_owner.wepay_account_id,
-           :amount => contribution.amount,
-           :short_description => "Contribution to Project ##{contribution.project.id} - #{contribution.project.name}",
-           :type => "GOODS",
-           :reference_id => "contribution-#{contribution.id}",
-           :app_fee => contribution.amount * (SETTINGS.fee_percentage/100.0),
-           :fee_payer => "Payee",
-           :redirect_uri => gateways_wepay_finish_url,
-           :auto_capture => 0,
-           :require_shipping => 0,
-      }
-    wp_params.merge!(:callback_uri => gateways_wepay_ipn_url) unless Rails.env.development?
-    logger.tagged("wepay params") { logger.info wp_params.inspect }
-    checkout = project_owner.wepay.get('/v2/checkout/create', :params => wp_params).parsed
-    logger.tagged("wepay response") { logger.info checkout.inspect }
-    if checkout["checkout_id"] > 0
-      contribution.update_attribute :wepay_checkout_id, checkout["checkout_id"]
-      redirect_to checkout["checkout_uri"]
-    else
-      flash[:error] = "Payment processing failed."
-      redirect_to contribution.project
+    if contribution.new?
+      project_owner = contribution.project.user
+      if contribution.wepay_checkout_id.nil?
+        wp_params = {
+               :account_id => project_owner.wepay_account_id,
+               :amount => contribution.amount,
+               :short_description => "Contribution to Project ##{contribution.project.id} - #{contribution.project.name}",
+               :type => "GOODS",
+               :reference_id => "contribution-#{contribution.id}",
+               :app_fee => contribution.amount * (SETTINGS.fee_percentage/100.0),
+               :fee_payer => "Payee",
+               :redirect_uri => gateways_wepay_finish_url,
+               :auto_capture => 0,
+               :require_shipping => 0,
+          }
+        wp_params.merge!(:callback_uri => gateways_wepay_ipn_url) unless Rails.env.development?
+        logger.tagged("wepay params") { logger.info wp_params.inspect }
+        checkout = project_owner.wepay.get('/v2/checkout/create', :params => wp_params).parsed
+        logger.tagged("wepay response") { logger.info checkout.inspect }
+        if checkout["checkout_id"] > 0
+          contribution.update_attribute :wepay_checkout_id, checkout["checkout_id"]
+          redirect_to checkout["checkout_uri"]
+        else
+          flash[:error] = "Payment processing failed. Please try again."
+          redirect_to contribution.project
+        end
+      else
+        wp_params = {:checkout_id => contribution.wepay_checkout_id }
+        logger.tagged("wepay params") { logger.info wp_params.inspect }
+        checkout = project_owner.wepay.get('/v2/checkout',
+                                           :params => wp_params).parsed
+        logger.tagged("wepay response") { logger.info checkout.inspect }
+        if checkout["state"] == "new"
+          redirect_to checkout["checkout_uri"]
+        else
+          flash[:error] = "This contribution has expired. Please try again."
+          redirect_to contribution.project
+        end
+      end
     end
   end
 
