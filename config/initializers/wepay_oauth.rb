011cc80e8364d6a622213b004d3695d6652c42cc
Don Park <don@donpark.org>
2012-07-13 10:11:08 -0700

    emergency oauth fix

--- config/initializers/wepay_oauth.rb
+++ config/initializers/wepay_oauth.rb
@@ -1,7 +1,9 @@
 WEPAY = OAuth2::Client.new(SETTINGS.payment_gateways.wepay.client.id,
                            SETTINGS.payment_gateways.wepay.client.secret,
-                                 {:site => SETTINGS.payment_gateways.wepay.sandbox ? "https://stage.wepayapi.com/" : "https://wepayapi.com/",
-                                  :authorize_url => "/v2/oauth2/authorize",
+                                 {:site => SETTINGS.payment_gateways.wepay.sandbox ?
+                                    "https://stage.wepayapi.com/" : "https://wepayapi.com/",
+                                  :authorize_url => SETTINGS.payment_gateways.wepay.sandbox ?
+                                    "https://stage.wepay.com/v2/oauth2/authorize" : "https://www.wepay.com/v2/oauth2/authorize",
                                   :token_url => "/v2/oauth2/token",
                                   :connection_opts => {
                                     :ssl => {:verify=> !Rails.env.development?}
