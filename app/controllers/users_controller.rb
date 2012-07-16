5dc4971d74e597888cbc227b7f9fbe32049d9908
Don Park <don@donpark.org>
2012-07-16 08:53:47 -0700

    edit your profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,32 @@ class UsersController < ApplicationController
     render :json => ret
   end
 
+  def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+      flash[:error] = "No permissions to edit user #{@user.username}"
+      redirect_to :root
+    end
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user == current_user
+      @user.update_attributes(params[:user])
+      if @user.valid?
+        flash[:success] = "Profile updated!"
+        redirect_to user_path(@user, :tab => "profile")
+      else
+        flash[:error] = "Update failed."
+        redirect_to edit_user_path(@user)
+      end
+    else
+      flash[:error] = "No permissions to edit user #{@user.username}"
+      redirect_to :root
+    end
   end
 end
