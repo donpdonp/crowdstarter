c02125b2807ff5069a71eb5e5e448c805beba5a6
Don Park <don@donpark.org>
2012-07-19 17:08:43 -0700

    capture fix

--- app/controllers/gateways/wepay_controller.rb
+++ app/controllers/gateways/wepay_controller.rb
@@ -86,7 +86,7 @@ class Gateways::WepayController < ApplicationController
         status = "OK"
       when "captured"
         contribution.capture!
-        activities.create(:detail => "Collected #{contrib.user.email} $#{contrib.amount}",
+        contribution.project.activities.create(:detail => "Collected #{contrib.user.email} $#{contrib.amount}",
                   :code => "capture",
                   :contribution => contrib)
         Notifications.delay(:queue => 'mailer').contribution_collected(contrib)
