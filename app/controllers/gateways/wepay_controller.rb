0520982b2260f7cc5c067b891ae9b801421c00d9
Don Park <don@donpark.org>
2012-07-23 11:25:30 -0700

    wepay_status here

--- app/controllers/gateways/wepay_controller.rb
+++ app/controllers/gateways/wepay_controller.rb
@@ -18,8 +18,7 @@ class Gateways::WepayController < ApplicationController
       else
         wp_params = {:checkout_id => contribution.wepay_checkout_id }
         logger.tagged("wepay params") { logger.info wp_params.inspect }
-        checkout = project_owner.wepay.get('/v2/checkout',
-                                           :params => wp_params).parsed
+        checkout = contribution.wepay_status
         logger.tagged("wepay response") { logger.info checkout.inspect }
         if checkout["state"] == "new"
           redirect_to checkout["checkout_uri"]
