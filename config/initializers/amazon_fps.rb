5a1ae16b1ac1c0e15d1dedea3983530478f9c089
Don Park <donp@donp.org>
2013-12-06 13:44:02 -0800

    remove outdated config, disable amazon bits

--- config/initializers/amazon_fps.rb
+++ config/initializers/amazon_fps.rb
@@ -1,3 +1,6 @@
-FPS = Boomerang.new( SETTINGS.payment_gateways.amazon.access_key,
-                     SETTINGS.payment_gateways.amazon.access_secret,
-                     SETTINGS.payment_gateways.amazon.sandbox)  # use sandbox (false sends to production)
+amazon_gateway = Gateway.where(:provider=>'amazon').first
+if amazon_gateway
+  FPS = Boomerang.new( amazon_gateway.access_key,
+                       amazon_gateway.access_secret,
+                       amazon_gateway.sandbox)  # use sandbox (false sends to production)
+end
