03517562630edad39792890b96ebcb4533ba818c
Don Park <donp@donp.org>
2015-03-10 09:56:49 -0700

    unknown https check thingy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,17 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, SETTINGS.facebook.app_id, SETTINGS.facebook.secret
 end
+
+if Rails.env.production?
+  module OmniAuth
+    module Strategy
+      def full_host
+        uri = URI.parse(request.url)
+        uri.path = ''
+        uri.query = nil
+        uri.port = (uri.scheme == 'https' ? 443 : 80)
+        uri.to_s
+      end
+    end
+  end
+end
