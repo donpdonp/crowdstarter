c10291a45739040add6cdae765d68bca47aa6c4b
Don Park <donp@donp.org>
2015-03-10 09:55:27 -0700

    obsolete amazon bit

--- config/initializers/amazon_fps.rb
+++
@@ -1,6 +0,0 @@
-amazon_gateway = Gateway.where(:provider=>'amazon').first
-if amazon_gateway
-  FPS = Boomerang.new( amazon_gateway.access_key,
-                       amazon_gateway.access_secret,
-                       amazon_gateway.sandbox)  # use sandbox (false sends to production)
-end
