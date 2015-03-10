1c11048e3845aa1bbca7b7dafb98ed235ce1e671
Don Park <donp@donp.org>
2015-03-10 11:32:22 -0700

    modern time handling

---
+++ app/assets/javascripts/time_util.js
@@ -0,0 +1,7 @@
+function time_fixups() {
+  $('time').each(function(){
+    var datetime = new XDate($(this).attr('datetime'))
+    var formatted = datetime.toString($(this).attr('data-format'))
+    $(this).html(formatted)
+  })
+}
